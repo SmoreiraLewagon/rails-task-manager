a0b0bb7266ae44a5aee4b209cd44da4d42f14abf
joao sampaio <joaomarcosemgp@gmail.com>
2021-05-25 17:03:34 -0300

    os sete comandos de CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema.define(version: 2021_05_25_150501) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.text "datails"
-    t.boolean "completed"
+    t.text "details"
+    t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
